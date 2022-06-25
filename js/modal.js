5c2e273eb2ea6bc321ece3275c64d9241f701624
Nexuslolz <a.klyosov@inbox.ru>
2022-06-25 14:28:34 +0500

    init: init repositiry

---
+++ js/modal.js
@@ -0,0 +1,25 @@
+const openLogin = document.querySelector('.login__btn');
+const loginModal = document.querySelector('.login__modal');
+const closeOverlay = document.querySelector('.page-main');
+
+
+
+openLogin.addEventListener('click',function(){
+    if(!(loginModal.classList.contains('login__modal_open'))){
+        loginModal.classList.add('login__modal_open');
+        openLogin.classList.add('login__btn_open');
+    } 
+    else {
+        loginModal.classList.remove('login__modal_open');
+    }
+});
+
+closeOverlay.addEventListener('click', function() {
+    if(loginModal.classList.contains('login__modal_open')){
+        loginModal.classList.remove('login__modal_open');
+        openLogin.classList.remove('login__btn_open')
+    }
+})
+
+
+
